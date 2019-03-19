3ad559aa3abed4c24b264928d0f65bd897b726d2
Дмитрий Старцев <dastartsev_2@edu.hse.ru>
2019-03-19 11:30:17 +0300

    kek

---
+++ M3_S5/T1/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Point
+{
+    public double x { get; set; }
+    public double y { get; set; }
+
+    public Point(double x, double y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double Distance(Point p)
+    {
+        return Math.Pow((p.x - this.x), 2) - Math.Pow((p.y - this.y), 2);
+    }
+}
+
+public class Circle
+{
+    public double rad { get; set; }
+    public Point Point_Center;
+    public Circle(double x, double y, double rad)
+    {
+        Point_Center = new Point(x, y);
+        this.rad = rad;
+    }
+
+    public override string ToString()
+    {
+        return $"Радиус = {rad:f3}. Координаты центра  {Point_Center.x:f3}, {Point_Center.y:f3}";
+    }
+}
+
+class Program
+{
+    public static double Input()
+    {
+        double a;
+
+        while (!double.TryParse(Console.ReadLine(), out a))
+        {
+            Console.WriteLine("Неверный ввод, введите вещественное число");
+        }
+
+        return a;
+    }
+
+    static void Main(string[] args)
+    {
+        do
+        {
+            List<Circle> kek = new List<Circle>();
+            double x, y, rad;
+
+            for (int i = 0; i < 4; i++)
+            {
+                x = Input(); y = Input(); rad = Input();
+                kek.Add(new Circle(x, y, rad));
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(kek[i]);
+            }
+            kek.Sort((a,b)=> (a.Point_Center.Distance(new Point(0, 0)) * a.rad)
+                  .CompareTo((b.Point_Center.Distance(new Point(0, 0)) * b.rad)));
+
+            for (int i = 0; i < 4; i++)
+            {
+                Console.WriteLine(kek[i]);
+            }
+
+        } while (Console.ReadKey(true).Key != ConsoleKey.Enter);
+    }
+}
+
---
+++ Startsev_2/Startsev_2/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Dot
+{
+    public double x, y;
+
+    public Dot(double xx, double yy)
+    {
+        x = xx;
+        y = yy;
+    }
+    
+}
+
+class Vector
+{
+    public Dot d1, d2;
+
+    public Vector(double x, double y, double x1, double y1)
+    {
+        this.d1 = new Dot(x, y);
+        this.d2 = new Dot(x1, y1);
+    }
+
+    public double Mod()
+    {
+        return Math.Sqrt(Math.Pow(d2.x - d1.x, 2) + Math.Pow(d2.y - d1.y, 2));
+    }
+
+}
+class Program
+{
+    public static int Input()
+    {
+        int a;
+
+        while (!int.TryParse(Console.ReadLine(), out a) || a <= 0)
+        {
+            Console.WriteLine("Неверный ввод, введите целое число большее 0");
+        }
+
+        return a;
+    }
+
+    struct MyStruct
+    {
+        int x;
+    }
+    static void Main(string[] args)
+    {
+        MyStruct s = new MyStruct();
+        Console.WriteLine(s.ToString());
+        int a = Console.Read();
+    }
+}
+
